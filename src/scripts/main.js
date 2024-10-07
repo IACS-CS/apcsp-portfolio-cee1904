9f937e6c22920e1d89f82e1bdf341c210f003498
cee1904 <itscece890@gmail.com>
2024-10-07 16:08:32 +0000

    fade in with aos practice-cik

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,15 @@
 import "../styles/styles.css";
 // Add imports here
 import "@picocss/pico/css/pico.min.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 console.log("What?");
 console.log("Hello world, from main.js!");
 if (typeof window !== "undefined") {
   // Put code that runs ONLY in the browser here -- this is most likely where
   // MOST of your code should go.
+   console.log("Hello world, from main.js!");
+   Aos.init();
   //
   // If you see a SSR error, try moving your code here!
   console.log("Hello world, browser :-)");
